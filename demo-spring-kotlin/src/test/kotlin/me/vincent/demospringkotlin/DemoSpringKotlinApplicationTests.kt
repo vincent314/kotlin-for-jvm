5039168252a61e1fe66cdc774a2012ab86aba537
Vincent Marmin <vincent.marmin@ext.adeo.com>
2021-12-04 20:53:40 +0100

    feat: init basic Spring application

---
+++ demo-spring-kotlin/src/test/kotlin/me/vincent/demospringkotlin/DemoSpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.vincent.demospringkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
